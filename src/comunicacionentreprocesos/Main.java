3dc28d4343ae061210247133f92228eb696dec5a
FVeraCoding <fveracoding@gmail.com>
2024-10-04 06:53:38 +0200

    Sincronización de Procesos basado en sistema de archivos.

--- src/comunicacionentreprocesos/Main.java
+++ src/comunicacionentreprocesos/Main.java
@@ -4,20 +4,38 @@ import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String[] args) {
-        File proceso1 = new File("/home/alumnadotarde/Escritorio", "proceso1.txt");
+        File proceso1 = new File("C:\\Users\\Fernando\\Desktop\\Procesos", "proceso1.txt");
         String[] datosProceso1 = leerArchivo(proceso1);
         
-        File proceso2 = new File("/home/alumnadotarde/Escritorio", "proceso2.txt");
+        File proceso2 = new File("C:\\Users\\Fernando\\Desktop\\Procesos", "proceso2.txt");
         String[] datosProceso2 = leerArchivo(proceso2);
+        
+        Process procesoUno;
+        Process procesoDos;
+        try {
+            procesoUno = Runtime.getRuntime().exec(datosProceso1);
+            int valorRetornoProceso1 = procesoUno.waitFor();
+            System.out.println(valorRetornoProceso1);
+            
+            if(valorRetornoProceso1 == 0){
+                System.out.println("El primer proceso ha terminado correctamente. Lanzando el segundo proceso.");
+                procesoDos = Runtime.getRuntime().exec(datosProceso2);
+            }else{
+                System.out.println("El primer proceso no ha terminado de la manera esperada. El segundo proceso no se lanzará.");
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         
     }
